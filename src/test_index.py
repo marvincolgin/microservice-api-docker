7521a333982bd606523d33d79836a7fe0a90cda9
Marvin Colgin <bpadmin@builtbp.com>
2019-09-21 15:32:11 -0700

    ADD: /compare tests

--- src/test_index.py
+++ src/test_index.py
@@ -35,10 +35,11 @@ class FlaskAPITests(unittest.TestCase):
         # on the specified path
         result = self.app.get('/generate')
 
+        # Make sure it's a json byte stream
         self.assertIsInstance(result.data, bytes)
-
         buf = result.data.decode('utf8').replace("'", '"')
         data = json.loads(buf)
+        self.assertIsInstance(data, dict)
 
         # Make sure it has a count field
         c = data.get('count', -1)
@@ -53,3 +54,54 @@ class FlaskAPITests(unittest.TestCase):
 
         # Make sure count(values) == data.count
         self.assertEqual(c, len(arr))
+
+    def test_compare_less(self):
+        val1 = 1
+        val2 = 100
+        cs = 1
+        result = self.app.get(f'/compare?val1={val1}&val2={val2}&cs={cs}')
+
+        # Make sure it's a json byte stream
+        self.assertIsInstance(result.data, bytes)
+        buf = result.data.decode('utf8').replace("'", '"')
+        data = json.loads(buf)
+        self.assertIsInstance(data, dict)
+
+        # Make sure it has 'result:bool'
+        r = data.get('result', -1)
+        self.assertIsInstance(r, bool)
+        self.assertEqual(r, True)
+
+    def test_compare_greater(self):
+        val1 = 500
+        val2 = 5
+        cs = 2
+        result = self.app.get(f'/compare?val1={val1}&val2={val2}&cs={cs}')
+
+        # Make sure it's a json byte stream
+        self.assertIsInstance(result.data, bytes)
+        buf = result.data.decode('utf8').replace("'", '"')
+        data = json.loads(buf)
+        self.assertIsInstance(data, dict)
+
+        # Make sure it has 'result:bool'
+        r = data.get('result', -1)
+        self.assertIsInstance(r, bool)
+        self.assertEqual(r, True)
+
+    def test_compare_equal(self):
+        val1 = 2
+        val2 = 2
+        cs = 3
+        result = self.app.get(f'/compare?val1={val1}&val2={val2}&cs={cs}')
+
+        # Make sure it's a json byte stream
+        self.assertIsInstance(result.data, bytes)
+        buf = result.data.decode('utf8').replace("'", '"')
+        data = json.loads(buf)
+        self.assertIsInstance(data, dict)
+
+        # Make sure it has 'result:bool'
+        r = data.get('result', -1)
+        self.assertIsInstance(r, bool)
+        self.assertEqual(r, True)
